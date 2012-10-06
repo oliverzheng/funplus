c156a9f83d7a41c2be84a5d588910fea4dfd934d
Oliver Zheng <gitcommits@oliverzheng.com>
2012-10-05 22:41:10 -0700

    Adding a web app.

---
+++ server.js
@@ -0,0 +1,96 @@
+var path = require('path');
+var express = require('express');
+var validate = require('jsonschema').validate;
+var nikeplus = require('./nikeplus');
+
+
+var app = express();
+
+app.configure(function() {
+	app.use(express.bodyParser());
+	app.use(express.static(path.join(__dirname, 'public')));
+});
+
+var schema = {
+	type: 'object',
+	properties: {
+		email: {
+			type: 'string',
+			required: true,
+		},
+		password: {
+			type: 'string',
+			required: true,
+		},
+		date: {
+			type: 'object',
+			required: true,
+			properties: {
+				year: {
+					type: 'integer',
+					required: true,
+					maximum: 9999
+				},
+				month: {
+					type: 'integer',
+					required: true,
+					minimum: 1,
+					maximum: 12,
+				},
+				day: {
+					type: 'integer',
+					required: true,
+					minimum: 1,
+					maximum: 31,
+				},
+				hour: {
+					type: 'integer',
+					required: true,
+					minimum: 0,
+					maximum: 23,
+				},
+				minute: {
+					type: 'integer',
+					required: true,
+					minimum: 0,
+					maximum: 59,
+				}
+			}
+		},
+		duration: {
+			type: 'integer',
+			required: true,
+			minimum: 0,
+		},
+		distance: {
+			type: 'number',
+			required: true,
+			minimum: 0,
+		},
+	},
+};
+
+app.post('/', function(req, res) {
+	// This is so disgusting. Just give me the damn raw data already.
+	var data = JSON.parse(Object.keys(req.body)[0]);
+	var validation = validate(data, schema);
+	if (validation.length !== 0) {
+		res.json(400, { success: false });
+		return;
+	}
+
+	var email = data.email;
+	var password = data.password;
+	var date = new Date(data.date.year, data.date.month, data.date.day, data.date.hour, data.date.minute);
+	var duration = data.duration;
+	var distance = data.distance;
+
+	nikeplus.addRun(email, password, date, duration, distance, function() {
+		res.json({ success: true });
+	}, function(err) {
+		res.json(400, { success: false, reason: err.reason });
+	});
+});
+
+app.listen(8000);
+console.log('Listening on port 8000');
